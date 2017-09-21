27405429f23db175844f967e7d3598ab7a383a68
Guillaume Moutier <guillaume.moutier@gmail.com>
2017-09-20 21:58:19 -0400

    Server Webpack

--- server.js
+++ server.js
@@ -1,34 +1,43 @@
-const fs = require('fs');
+/* eslint no-console: 0 */
+
+const path = require('path');
 const express = require('express');
-const expressStaticGzip = require("express-static-gzip");
-const path = require("path");
-
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+// const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./conf/webpack.config.js');
+const os = require('os');
+const port = 8080;
 const app = express();
-var indexFile;
-
-// Pour que la page racine utilise la cache du index.html
-app.use(/^\/$/, function (req, res, next) {
-    res.send(indexFile);
-});
-
-// Pour supporter les fichiers .gz
-app.use("/", expressStaticGzip(path.join(__dirname, 'dist')));
-
-// Pour gérer le 404 - on retourne index.html et on laisse l'application gérer l'erreur
-app.use(function (req, res, next) {
-    res.send(indexFile);
-});
-
-// Pour mettre le fichier index.html en cache
-fs.readFile(path.join(__dirname, 'dist/index.html'), 'utf8', function (err, data) {
-    if (err) {
-        console.error(err);
-        process.exit(1);
+const compiler = webpack(config);
+const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
     }
+  });
 
-    indexFile = data;
+app.use(middleware);
+//   app.use(webpackHotMiddleware(compiler));
 
-    app.listen(8083, function () {
-        console.log('Listening on port 8083...')
-    });
+  app.get('/hostname', function (req, res) {
+      res.send(os.hostname());
+  })
+  app.get('*', function response(req, res) {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    res.end();
+  });
+
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> ðŸŒŽ Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
